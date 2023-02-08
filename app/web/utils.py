c9e824d44d8bac7cf423efb214220cb5247b3ef4
nlif <m.payunen@gmail.com>
2023-02-08 21:44:40 +0300

    feat(web): implement error_json_response

--- app/web/utils.py
+++ app/web/utils.py
@@ -3,6 +3,8 @@ from typing import Any, Optional
 from aiohttp.web import json_response as aiohttp_json_response
 from aiohttp.web_response import Response
 
+from app.web.middlewares import HTTP_ERROR_CODES
+
 
 def json_response(data: Any = None, status: str = "ok") -> Response:
     if data is None:
@@ -22,11 +24,11 @@ def error_json_response(
     data: Optional[dict] = None,
 ):
     if message is None:
-        message = ""
+        # TODO: Think of another message
+        message = "Message was not setted"
     if data is None:
         data = {}
-    # TODO: Handle key error when accesing HTTP_ERROR_CODES dict
-    return aiohttp_json_response(data={"status": status,
+    return aiohttp_json_response(data={"status": HTTP_ERROR_CODES[http_status],
                                        "message": message,
                                        "data": data},
                                  status=http_status)
