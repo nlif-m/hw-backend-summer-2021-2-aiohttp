15f8983fc136b6b512b7c15be3be16a791b1f218
nlif <m.payunen@gmail.com>
2023-02-09 22:11:50 +0300

    feat(quiz): implement ThemeAddView

--- app/quiz/views.py
+++ app/quiz/views.py
@@ -1,18 +1,25 @@
+from aiohttp_apispec import json_schema, docs, response_schema, request_schema
+from aiohttp.web_exceptions import HTTPConflict
+
 from app.quiz.schemes import (
     ThemeSchema,
 )
 from app.web.app import View
 from app.web.utils import json_response
+from app.web.mixins import AuthRequiredMixin
 
 
 # TODO: добавить проверку авторизации для этого View
-class ThemeAddView(View):
+class ThemeAddView(AuthRequiredMixin, View):
     # TODO: добавить валидацию с помощью aiohttp-apispec и marshmallow-схем
+    @docs()
+    @response_schema(ThemeSchema)
+    @request_schema(ThemeSchema)
     async def post(self):
-        title = (await self.request.json())[
-            "title"
-        ]  # TODO: заменить на self.data["title"] после внедрения валидации
-        # TODO: проверять, что не существует темы с таким же именем, отдавать 409 если существует
+        title = self.data["title"]
+        existed_theme = await self.store.quizzes.get_theme_by_title(title)
+        if existed_theme is not None:
+            raise HTTPConflict(reason="theme already exists")
         theme = await self.store.quizzes.create_theme(title=title)
         return json_response(data=ThemeSchema().dump(theme))
 
