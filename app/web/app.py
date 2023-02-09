26eac2b2e83e0b8a9ae95df05d59c22eb4da69d6
nlif <m.payunen@gmail.com>
2023-02-09 20:00:37 +0300

    refactor(web): setup aiohttp_session in separate setup_...

--- app/web/app.py
+++ app/web/app.py
@@ -7,6 +7,8 @@ from aiohttp.web import (
 )
 
 from aiohttp_apispec import setup_aiohttp_apispec
+from aiohttp_session import setup as setup_aiohttp_session
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
 
 from app.admin.models import Admin
 from app.store import setup_store, Store
@@ -52,7 +54,15 @@ def setup_app(config_path: str) -> Application:
     setup_logging(app)
     setup_config(app, config_path)
     setup_routes(app)
-    setup_aiohttp_apispec(app, title='Quiz Application', url='/docs/json', swagger_path='/docs')
+    setup_aiohttp_session(
+        app,
+        storage=EncryptedCookieStorage(
+            secret_key=app.config.session.secret_key, secure=False
+        ),
+    )
+    setup_aiohttp_apispec(
+        app, title="Quiz Application", url="/docs/json", swagger_path="/docs"
+    )
     setup_middlewares(app)
     setup_store(app)
     return app
