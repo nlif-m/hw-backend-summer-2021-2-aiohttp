70eab098b500ee96871ce22ec04f6853c6491be5
nlif <m.payunen@gmail.com>
2023-02-11 16:53:07 +0300

    feat(store): enable VkApiAccessor and BotManager in store initialize

--- app/store/__init__.py
+++ app/store/__init__.py
@@ -10,9 +10,13 @@ class Store:
     def __init__(self, app: "Application"):
         from app.store.quiz.accessor import QuizAccessor
         from app.store.admin.accessor import AdminAccessor
+        from app.store.vk_api.accessor import VkApiAccessor
+        from app.store.bot.manager import BotManager
 
         self.quizzes = QuizAccessor(app)
         self.admins = AdminAccessor(app)
+        self.vkApi = VkApiAccessor(app)
+        self.bots_manager = BotManager(app)
 
 
 def setup_store(app: "Application"):
