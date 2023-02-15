b919b1c7ca502447ef0181163d795d44078d4ce9
nlif <m.payunen@gmail.com>
2023-02-15 21:39:44 +0300

    feat(vk_api): implement poller

--- app/store/vk_api/poller.py
+++ app/store/vk_api/poller.py
@@ -1,6 +1,9 @@
-from asyncio import Task
+from asyncio import Task, create_task, wait
 from typing import Optional
 
+from aiohttp.client_exceptions import ClientOSError
+
+from app.store.vk_api.dataclasses import Update, UpdateObject, UpdateMessage
 from app.store import Store
 
 
@@ -11,12 +14,31 @@ class Poller:
         self.poll_task: Optional[Task] = None
 
     async def start(self):
-        # TODO: добавить asyncio Task на запуск poll
-        raise NotImplementedError
+        self.is_running = True
+        self.poll_task = create_task(self.poll())
 
     async def stop(self):
-        # TODO: gracefully завершить Poller
-        raise NotImplementedError
+        self.is_running = False
+        if self.poll_task:
+            await wait([self.poll_task], timeout=30)
 
     async def poll(self):
-        raise NotImplementedError
+        while self.is_running:
+            try:
+                raw_updates = await self.store.vk_api.poll()
+            except ClientOSError:
+                continue
+            updates = [
+                Update(
+                    type=raw_update["type"],
+                    object=UpdateObject(
+                        UpdateMessage(
+                            raw_update["object"]["message"]["from_id"],
+                            raw_update["object"]["message"]["text"],
+                            raw_update["object"]["message"]["id"],
+                        )
+                    ),
+                )
+                for raw_update in raw_updates
+            ]
+            await self.store.bots_manager.handle_updates(updates)
