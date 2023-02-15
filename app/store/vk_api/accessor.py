43f785053ef492f44fadd5568d64dd3ccf5932a2
nlif <m.payunen@gmail.com>
2023-02-15 21:40:02 +0300

    feat(vk_api): implement accessor

--- app/store/vk_api/accessor.py
+++ app/store/vk_api/accessor.py
@@ -1,3 +1,4 @@
+import random
 import typing
 from typing import Optional
 
@@ -21,14 +22,14 @@ class VkApiAccessor(BaseAccessor):
         self.ts: Optional[int] = None
 
     async def connect(self, app: "Application"):
-        # TODO: добавить создание aiohttp ClientSession,
-        #  получить данные о long poll сервере с помощью метода groups.getLongPollServer
-        #  вызвать метод start у Poller
-        raise NotImplementedError
+        self.session = ClientSession()
+        await self._get_long_poll_service()
+        self.poller = Poller(self.app.store)
+        await self.poller.start()
 
     async def disconnect(self, app: "Application"):
-        # TODO: закрыть сессию и завершить поллер
-        raise NotImplementedError
+        await self.session.close()
+        await self.poller.stop()
 
     @staticmethod
     def _build_query(host: str, method: str, params: dict) -> str:
@@ -39,10 +40,43 @@ class VkApiAccessor(BaseAccessor):
         return url
 
     async def _get_long_poll_service(self):
-        raise NotImplementedError
+        url = self._build_query(
+            host="https://api.vk.com/method",
+            method="/groups.getLongPollServer",
+            params={
+                "group_id": self.app.config.bot.group_id,
+                "access_token": self.app.config.bot.token,
+            },
+        )
 
-    async def poll(self):
-        raise NotImplementedError
+        async with self.session.get(url) as resp:
+            data = (await resp.json())["response"]
+            self.app.logger.info(data)
+            self.ts = data["ts"]
+            self.server = data["server"]
+            self.key = data["key"]
+
+    async def poll(self) -> dict:
+        self.app.logger.info("new poll request")
+        url = f"{self.server}?act=a_check&key={self.key}&ts={self.ts}&wait=25"
+        self.app.logger.info(f"long poll with {url}")
+        async with self.session.get(url) as resp:
+            data = await resp.json()
+            self.app.logger.info(f"poll:{data}")
+            self.ts = data["ts"]
+            return data["updates"]
 
     async def send_message(self, message: Message) -> None:
-        raise NotImplementedError
+        url = self._build_query(
+            host="https://api.vk.com/method",
+            method="/messages.send",
+            params={
+                "peer_id": message.user_id,
+                "message": message.text,
+                "access_token": self.app.config.bot.token,
+                "random_id": random.randint(0, 10000000),
+            },
+        )
+        async with self.session.get(url) as resp:
+            data = await resp.json()
+            self.app.logger.info(data)
